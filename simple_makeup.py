76583dfa056ac77ed95ab2e95ce4249b02fa9f91
chendinghao <171196162@qq.com>
2017-02-23 15:52:12 +0800

    Create simple_makeup.py

---
+++ simple_makeup.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+import sys
+import re
+
+from util import *
+
+print '<html><head><title>...</title><body>'
+
+title = True
+for block in blocks(sys.stdin):
+    block = re.sub(r'\*(.+?)\*', r'<em>\1<\em>', block)
+    if title:
+        print '<h1>'
+        print block
+        print '</h1>'
+        title = False
+    else:
+        print '<p>'
+        print block
+        print '</p>'
+
+print '</body>\1</html>'
